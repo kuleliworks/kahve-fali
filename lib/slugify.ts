6a78579a7bd9f739e1ad54a1ed1143ca5f9a3483
kuleliworks <kuleliagency@gmail.com>
2025-08-23 02:04:39 +0300

    Create slugify.ts

---
+++ lib/slugify.ts
@@ -0,0 +1,11 @@
+// lib/slugify.ts
+export function slugify(input: string) {
+  return input
+    .toLowerCase()
+    .normalize("NFKD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[^a-z0-9\s-]/g, "")
+    .trim()
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-");
+}
